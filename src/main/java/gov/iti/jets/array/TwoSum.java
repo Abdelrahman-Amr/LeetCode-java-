e55ccfba09a1e32cea20f71fec0f4ee793f3d048
Abdelrahman-Amr <abdozyed2199@gmail.com>
2023-04-27 09:02:13 +0200

    BestTimeToBuyAndSellStock

--- src/main/java/gov/iti/jets/array/TwoSum.java
+++ src/main/java/gov/iti/jets/array/TwoSum.java
@@ -1,11 +1,13 @@
 package gov.iti.jets.array;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 public class TwoSum {
 
     public static int[] twoSum(int[] nums, int target) {
+
         Map<Integer, Integer> freq= new HashMap<>();
 
         int result[] = new int[2];
