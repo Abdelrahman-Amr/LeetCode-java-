9a410f8d416ad47e4661a27cf838b3f369302e2d
Abdelrahman-Amr <abdozyed2199@gmail.com>
2023-05-05 01:22:25 +0300

    SearchInRotatedSortedArray

---
+++ src/main/java/gov/iti/jets/array/SearchInRotatedSortedArray.java
@@ -0,0 +1,16 @@
+package gov.iti.jets.array;
+
+public class SearchInRotatedSortedArray {
+    public int search(int[] nums, int target) {
+        int result = -1;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i] == target)
+            {
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
+}
